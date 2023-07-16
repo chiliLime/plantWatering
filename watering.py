f2b28d25c775abb93f31f2d342f5f93e2bbc5343
chiliLime <jaimeson17@gmail.com>
2023-07-16 13:34:16 -0700

    Update watering.py

--- watering.py
+++ watering.py
@@ -14,8 +14,13 @@ def readData(channel):
       data = ((adc[1]&3) << 8) + adc[2]
       return data
 
+# variables for moisture calibration
 pinPump = 4                               # GPIO pin of pump
-needsWater = 630                          # sensor value for dry air
+needsWater = 630                          # sensor value to trigger watering
+needsWaterPercent = 40                    # percentage to trigger watering; 100 = waterValue
+dryAirValue = 775                         # sensor value for dry air
+waterValue = 300                          # aensor value immersed in water
+moistureRange = dryAirValue - waterValue  # range of sensor values
 
 # general GPIO settings
 GPIO.setwarnings(False)                   # ignore warnings (irrelevant here)
@@ -32,9 +37,12 @@ currentTime = datetime.datetime.now()
 f.write(str(currentTime) + ":\n")
 
 # 450 = 780 - 330, moisture in %
-f.write("Current moisture: " + str(round((moisture-330) / 450 * 100, 2)) +
+# f.write("Current moisture: " + str(round((moisture-330) / 450 * 100, 2)) + "% (" + str(moisture) + ")\n")
+moisturePercent = (1 - ((moisture - waterValue) / moistureRange)) * 100
+f.write("Current moisture: " + str(round(moisturePercent) +
 "% (" + str(moisture) + ")\n")
 
+# FOLLOWING NEEDS UPDATE
 # if plants are to dry, start pumping and record the moisture in file
 if moisture > needsWater:
     t_end = time.time() + 4               # pump runs 4 seconds
